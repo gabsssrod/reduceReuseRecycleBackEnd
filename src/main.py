65c384d326a10ea6729590536eebfa1e50b44f6f
gabsrodriguez <gmederos95@gmail.com>
2020-01-22 19:09:39 +0000

    first commit

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
-from models import db
+from models import db, Users, Objects, Resource_Centers,Days
 #from models import Person
 
 app = Flask(__name__)
@@ -28,14 +28,75 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+@app.route('/users', methods=['GET'])
+    def my_users():
+        if request.method == 'GET':
+            users = Users.query.all()
+            if not users:
+                return jsonify({'msg': 'User not found'}), 404
+
+            return jsonify( [x.serialize() for x in users] ), 200
+
+
 @app.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+    def handle_hello():
 
-    response_body = {
-        "hello": "world"
-    }
+        response_body = {
+            "hello": "world"
+        }
 
-    return jsonify(response_body), 200
+        return jsonify(response_body), 200
+
+@app.route('/add_user', methods=['POST'])
+    def add_user():
+        if request.method == 'POST':
+            body = request.get_json()
+
+            db.session.add(Users(
+                first_name = body["first_name"],
+                last_name = body["last_name"],
+                email = body["email"],
+                password = body["password"],
+                zip = body["zip"]
+            ))
+            
+            db.sessions.commit()
+            return jsonify({
+                'msg': 'User Added!'
+            })
+
+@app.route('/add_days', methods=['POST'])
+def add_day():
+    if request.method == 'POST':
+        body = request.get_json()
+
+        db.session.add(Days(
+            first_day = body["first_day"],
+            second_day = body["second_day"],
+            user_id = body["user_id"]
+        ))
+        
+        db.session.commit()
+        return jsonify({
+            'msg': 'Day Added!'
+        })
+
+
+# @app.route('/edit_user', methods=['PUT'])
+# def edit_user():
+#     body = request.get_json()
+#     if request.method == 'PUT':
+#         first_name = body["first_name"],
+#         last_name = body["last_name"],
+#         email = body["email"],
+#         password = body["password"],
+#         zip = body["zip"]
+
+#         db.sessions.commit()
+#         return jsonify({
+#             'msg': 'Info Updated!'
+#         })
+        
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -2,16 +2,84 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-# class Person(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(80), unique=True, nullable=False)
-#     email = db.Column(db.String(120), unique=True, nullable=False)
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(80), nullable=False)
+    last_name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    zip = db.Column(db.Integer(), nullable=False)
+    
+    def __repr__(self):
+        return '<Users %r>' % self.first_name
 
-#     def __repr__(self):
-#         return '<Person %r>' % self.username
+    def serialize(self):
+        return {
+        "first_name": self.first_name,
+        "last_name": self.last_name,
+        "email": self.email,
+        "password": self.password,
+        "zip": self.zip
+        }
 
-#     def serialize(self):
-#         return {
-#             "username": self.username,
-#             "email": self.email
-#         }
+class Objects(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    object_name = db.Column(db.String(68), nullable=False)
+    recycle = db.Column(db.String(68), nullable=False)
+    reuse = db.Column(db.String(68), nullable=False)
+    reduce = db.Column(db.String(68), nullable=False)
+
+    def __repr__(self):
+        return '<Objects %r>' % self.object_name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "objectname": self.object_name,
+            "recycle": self.recycle,
+            "reuse": self.reuse,
+            "reduce": self.reduce
+        }
+    
+class Resource_Centers(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    center_name = db.Column(db.String(200), nullable=False)
+    address = db.Column(db.Integer, nullable=False)
+    city = db.Column(db.String(100), nullable=False)
+    state = db.Column(db.String(30), nullable=False)
+    zip = db.Column(db.Integer, nullable=False)
+    hours = db.Column(db.Integer, nullable=False)
+    phone_number = db.Column(db.Integer, nullable=False)
+    website = db.Column(db.String(200), nullable=True)
+    types = db.Column(db.String(200), nullable=False)
+
+    def __repr__(self):
+        return '<Resource_Centers %r>' % self.center_name
+
+    def serialize(self):
+        return {
+            "center_name": self.center_name,
+            "address": self.address,
+            "city": self.city,
+            "state": self.state,
+            "zip": self.zip,
+            "hours": self.hours,
+            "phone_number": self.phone_number,
+            "website": self.website,
+            "types": self.types
+        }
+
+class Days(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    first_day = db.Column(db.String(200), nullable=False)
+    second_day = db.Column(db.String(200), nullable=False)
+
+    def __repr__(self):
+        return '<Days %r>' % self.first_day
+
+    def serialize(self):
+        return {
+            "first_day": self.first_day,
+            "secon_day": self.secon_day
+        }
